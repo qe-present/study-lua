15985306d3eb8b1ebe5c4940cdde6d3a0655270b
qe-present <2664481691@qq.com>
2026-08-29 11:46:37 +0800

    提交

---
+++ 02-type/main.lua
@@ -0,0 +1,10 @@
+-- 这是单行注释
+--[[
+多行注释
+多行注释
+]]
+local name = "小明" --字符串
+local age = 18 -- 数字 lua里面整数小数都是number
+local is_student = true --布尔值
+local nothing = nil -- nil 表示什么都没有
+print(name,age,is_student,nothing)
