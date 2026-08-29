54b3e633b9c6d5451e909538bd02804ac62c8164
qe-present <2664481691@qq.com>
2026-08-29 13:22:25 +0800

    3

--- script/get_message.bash
+++ script/get_message.bash
@@ -4,6 +4,7 @@ set -e
 input="$1"
 msg=$(echo "$input"|xargs)
 if [[ "$msg" =~ ^[0-9]+$ ]]; then
+    [[ "$msg" =~ ^[0-9]$ ]] && msg="0$msg"
     dir=$(find . -maxdepth 1 -type d -name "${msg}-*"|head -n 1)
     if [ -n "$dir" ]; then
           echo "$dir"                    # 找到，输出目录名
