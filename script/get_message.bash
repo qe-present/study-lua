8baf64136fccd6da96d7fa70cae1772cc240a46c
qe-present <2664481691@qq.com>
2026-08-29 12:51:04 +0800

    02

--- script/get_message.bash
+++ script/get_message.bash
@@ -7,12 +7,10 @@ if [[ "$msg" =~ ^[0-9]+$ ]]; then
     dir=$(find . -maxdepth 1 -type d -name "${msg}-*"|head -n 1)
     if [ -n "$dir" ]; then
           echo "$dir"                    # 找到，输出目录名
-      else
-          echo "错误: 找不到 ${msg}-* 目录" >&2
-          exit 1
-      fi
-  else
-
+    else
+        echo "gg"
+        exit 1
+    fi
 else
     echo "gg"
     exit 1
